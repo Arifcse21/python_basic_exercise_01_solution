c2aa6663ef6cfa5d5bd4e0dfd7f2796764e593ec
ARIF <lone_wolf@ASUS.localdomain>
2020-05-09 17:14:09 +0600

    compute payment with overtime solution

---
+++ 02_compute_payment_with_over_time.py
@@ -0,0 +1,24 @@
+"""
+Write a program that will take hours and rate per hour as input and will compute the total payment.
+Here your program will give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+
+Example:
+Enter Hours: 45
+Enter Rate: 10
+Pay: 475.00
+
+"""
+
+# take hours and rate as input
+hours = int(input('Enter Hours: '))
+rate = float(input('Enter Rate:'))
+pay = 0
+
+if hours>40:
+    r=hours-40
+    pay=(40*rate)+(r*rate)*1.5
+else:
+    pay=hours*rate
+
+
+print('Pay: {:.2f}'.format(pay))
